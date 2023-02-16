1efda6b39063c82efdf9b28959353159c6834746
Anna Moura Sobral <annamourasobral@gmail.com>
2023-02-16 21:11:12 +0100

    factorial on recursive

--- Data Structure for Smart Systems/FatorialP.java
+++ Data Structure for Smart Systems/FatorialP.java
@@ -1,58 +1,82 @@
-import javax.swing.;
+// import javax.swing.;
+
+// class FatorialP {
+
+//   static void fatP (int n, int x, int f)
+
+//     {
+
+//       if (x == 0 || x == 1)  {
+
+//           System.out.println("The factorial of " + n + " is " + f);
+//         }
+
+//       else
+
+//         {
+
+//           fatP(n, x-1, fx);
+
+//         }
+
+//     }
+
+//     public static void main (String arg [ ])
+
+//         {
+
+//             int nro;
 
  
 
-class FatorialP {
+//             nro = Integer.parseInt(JOptionPane.showInputDialog(“Type a number that you want to know the factorial”));
 
-  static void fatP (int n, int x, int f)
+//             if (nro < 0)
 
-    {
+//                {
 
-      if (x == 0 || x == 1)  {
+//                   System.out.println (“This value is not valid, the number must be equal or bigger than zero”);
 
-          System.out.println("The factorial of " + n + " is " + f);
+//                   System.exit(0);
+
+//                }
+
+//             else
+
+//                {
+
+//                   fatP(nro, nro, 1);
+
+//                }
+
+//             System.exit(0);
+
+//          }
+
+//    }
+
+import javax.swing.*;
+
+class Factorial {
+
+  static int fat (int n) {
+
+      int f;
+
+      if (n == 0)  {
+
+          return 1; 
+
         }
 
       else
 
         {
 
-          fatP(n, x-1, fx);
+        
+          f = n * fat(n-1); 
+          return f;
 
         }
 
     }
-
-    public static void main (String arg [ ])
-
-        {
-
-            int nro;
-
- 
-
-            nro = Integer.parseInt(JOptionPane.showInputDialog(“Type a number that you want to know the factorial”));
-
-            if (nro < 0)
-
-               {
-
-                  System.out.println (“This value is not valid, the number must be equal or bigger than zero”);
-
-                  System.exit(0);
-
-               }
-
-            else
-
-               {
-
-                  fatP(nro, nro, 1);
-
-               }
-
-            System.exit(0);
-
-         }
-
-   }
